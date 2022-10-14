cfbc2aa22f3a4d4997cc2fd198e834635a2fea1e
IgBuS <i.b@msulab.ru>
2022-10-14 14:38:15 +0300

    main page fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,24 +2,9 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    @auth
-                    {{ __('You are logged in!') }}
-                    @endauth
-                </div>
-            </div>
-        </div>
+    <div class="col-md-8">
+        <h1>Привет от Хекслета!</h1>
+        <p>Это простой менеджер задач на Laravel</p>
     </div>
 </div>
 @endsection
