7f443af040259533b71360de78a0412e9ab55cc0
IgBuS <i.b@msulab.ru>
2022-10-17 16:25:58 +0300

    fix linter

--- lang/ru/flash.php
+++ lang/ru/flash.php
@@ -15,7 +15,7 @@ return [
     'status_delete_error' => "Не удалось удалить статус",
     'status_create_name_unique' => "Статус с таким именем уже существует",
     'create_name_require' => "Это обязательное поле",
-    'label_create_name_unique'=>'Метка с таким именем уже существует',
+    'label_create_name_unique' => 'Метка с таким именем уже существует',
     'task_create_name_unique' => 'Задача с таким именем уже существует'
 
 ];
